11f0a269ff1f519a8be2e880074724410d8ccf07
raashid1 <31041961+raashid1@users.noreply.github.com>
2017-08-15 23:39:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res) {
+    res.send('Article one is reqtd and will be served here');
+});
+app.get('/article-two',function(req,res) {
+    res.send('Article 2 is reqtd and will be served here');
+});
+app.get('/article-three',function(req,res) {
+    res.send('Article 3 is reqtd and will be served here');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
